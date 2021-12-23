40a2984ab15e5902f786f37a95b29a6d2320e0dd
punit355743 <punit.java.dixit@gmail.com>
2021-12-23 11:17:20 +0530

    added servcie worker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import OneSignal from "react-onesignal";
+import "./App.css";
 
 function App() {
+
+  const [heading,setHeading] = useState('');
+  const [content,setContent] = useState('');
+
+  useEffect(() => {
+    OneSignal.init({
+      appId: "7b91a212-d72b-4465-9c90-ede7bcf0ed3b"
+    });
+
+    OneSignal.addListenerForNotificationOpened(() => {
+      console.log("hi");
+    });
+
+    OneSignal.on("addListenerForNotificationOpened", function (event) {
+      console.log("Notification Clicked", event);
+    });
+
+    OneSignal.on("notificationDisplay", function (event) {
+      console.log("OneSignal notification displayed:", event);
+      setHeading(event.heading);
+      setContent(event.content);
+    });
+    OneSignal.on("notificationDismiss", function (event) {
+      console.log("OneSignal notification dismissed:", event);
+    });
+  }, []);
+
+  // OneSignal.on('notificationDisplay', function(event) {
+  //   console.warn('OneSignal notification displayed:', event);
+  // });
+
+  const onHandleTag = (tag) => {
+    console.log("Tagging");
+    OneSignal.sendTag("tech", tag).then(() => {
+      console.log("tagged");
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> React one signal push notification demo</h1>
+      <div className='mycontent-style'>
+      <h2><b>Title:</b> {heading} </h2>
+      <h2><b>Message:</b> {content} </h2>
+      </div>
+      <button onClick={() => onHandleTag("react")}>Click</button>
     </div>
   );
 }
